1c64a905461f9c36bd191b8123c5f6d6f4e5e5b8
Aravind karthik <aravind.karthik@go-jek.com>
2017-04-02 00:55:23 +0530

    [ARAVIND KARTHIK][Initial commit] CREATE A SIMPLE ROLL A BALL GAME

---
+++ Assets/scripts/PlayerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	public float SPEED;
+	public Text scoreText;
+	public Text gameOverText;
+	private int score = 0;
+
+	void Start() 
+	{
+		gameOverText.gameObject.SetActive (false);
+		rigidBody = GetComponent<Rigidbody> ();
+		score = 0;
+		setScore();
+	}
+
+	void FixedUpdate() 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidBody.AddForce (movement * SPEED);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("pickup")) 
+		{
+			other.gameObject.SetActive(false);
+			score++; 
+			setScore();
+			if (score >= 17) {
+				gameOverText.gameObject.SetActive (true);
+			}
+		}
+	}
+
+	void setScore(){
+		scoreText.text = "Score : " + score.ToString();
+
+	}
+}
