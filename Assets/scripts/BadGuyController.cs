10da249991c2365c9746ebe055fade71e984bd81
Aravind karthik <aravind.karthik@go-jek.com>
2017-04-03 23:45:45 +0530

    added gitignore and fixed minor bugs

---
+++ Assets/scripts/BadGuyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuyController : MonoBehaviour {
+	Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+}
